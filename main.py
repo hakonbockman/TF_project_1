dcb3b05f8b6615fdd423d05f548d5dc9d60e7a78
Håkon <hakonbockman@gmail.com>
2021-07-31 21:33:28 +0200

    Added plots
    It is now able to train, validate and plot every sub-data-set

--- main.py
+++ main.py
@@ -13,6 +13,27 @@ import matplotlib.pyplot as plt
 
 # LOAD
 
+#module_spec = hub.load_module_spec("./model_files/")
+#height, width = hub.get_expected_image_size(module_spec)
+#images = ...  # A batch of images with shape [batch_size, height, width, 3].
+#module = hub.Module(module_spec)
+#features = module(images)   # A batch with shape [batch_size, num_features].
+
+def return_image_path_dict():
+    return {
+        'image_path_IR'                         : '../saue bilder/Combined/IR_originale/',
+        'img_path_IR_duplicate_STRICT'          : '../saue bilder/Combined/IR_removed_duplicate_withOUT_blurry/removed_duplicate_STRICT/',
+        'img_path_IR_duplicate_BASIC'           : '../saue bilder/Combined/IR_removed_duplicate_withOUT_blurry/removed_duplicate_BASIC/',
+        #'img_path_IR_duplicate_LOOSE'           : '../saue bilder/Combined/IR_removed_duplicate_withOUT_blurry/removed_duplicate_LOOSE/',
+        'img_path_IR_BLURRY_duplicate_STRICT'   : '../saue bilder/Combined/IR_removed_duplicate_with_blurry/IR_removed_duplicate_STRICT/',
+        'img_path_IR_BLURRY_duplicate_BASIC'    : '../saue bilder/Combined/IR_removed_duplicate_with_blurry/IR_removed_duplicate_BASIC/',
+        'img_path_IR_BLURRY_duplicate_LOOSE'    : '../saue bilder/Combined/IR_removed_duplicate_with_blurry/IR_removed_duplicate_LOOSE/',
+        'img_path_VISUAL'                       : '../saue bilder/Combined/Visual/',
+        'img_path_VISUAL_duplicate_STRICT'      : '../saue bilder/Combined/Visual_removed_duplicate_STRICT/',
+        'img_path_VISUAL_duplicate_BASIC'       : '../saue bilder/Combined/Visual_removed_duplicate_BASIC/',
+        'img_path_VISUAL_duplicate_LOOSE'       : '../saue bilder/Combined/Visual_removed_duplicate_LOOSE/'
+        }
+
 def load_data_tflite_model_maker(path, split_trainrest_ratio=0.9, split_testval_ratio=0.5):
     data = DataLoader.from_folder(path, shuffle=True)
     train_data, rest_data = data.split(split_trainrest_ratio)
@@ -67,6 +88,36 @@ def build_model(
 
     return model
 
+# Evaluate 
+def evaluate_model(test_data, model):
+    loss, accuracy = model.evaluate(test_data)
+    return loss, accuracy
+
+# Export model
+def export_model(model, export_dir, overwrite, export_format, label_filename, save_model_filename):
+    model.export(
+        export_dir=export_dir,        #, tflite_filname='sheep_classifcation_model.tflite'
+        export_format=export_format, 
+        label_filename=label_filename,
+        overwrite=overwrite,
+        save_model_filename=save_model_filename
+    )
+
+# Alternative Evaluation method #TODO: Not working
+def alternative_evaluation_tflite_mode(model, test_data):
+    model.evaluate_tflite('model.tflite', test_data)
+
+
+
+# Load
+# Build
+
+# Evaluate
+
+# Export model
+
+
+# plot classified images
 def plot_classified_images(model, test_data):
 
     # Helper function that returns "red or black" depending on if its two input parameters mathces or not
@@ -98,56 +149,54 @@ def plot_classified_images(model, test_data):
         plt.xlabel('Predicted: %s' % predict_label)
     plt.show()
 
-# Evaluate 
-def evaluate_model(test_data, model):
-    loss, accuracy = model.evaluate(test_data)
-    return loss, accuracy
+# plot model
+def draw_learning_curves(history, model_title):
+    #plot_title = str(path.split("/")[-2])
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    figure = plt.figure(figsize=(8, 8))
+    plt.ion()
+    plt.subplot(2, 1, 1)
+    plt.plot(acc, label='Training Accuracy')
+    plt.plot(val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.ylabel('Accuracy')
+    plt.ylim([min(plt.ylim()), 1])
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.ylabel('Cross Entropy')
+    plt.ylim([0,1.0])
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    #plt.close(figure)
+    plt.savefig("plots/"+ model_title)
+    #plt.show()
 
 
-# Export model
-def export_model(model, export_dir, overwrite, export_format, label_filename, save_model_filename):
-    model.export(
-        export_dir=export_dir,        #, tflite_filname='sheep_classifcation_model.tflite'
-        export_format=export_format, 
-        label_filename=label_filename,
-        overwrite=overwrite,
-        save_model_filename=save_model_filename
-    )
-
-# Alternative Evaluation method #TODO: Not working
-def alternative_evaluation_tflite_mode(model, test_data):
-    model.evaluate_tflite('model.tflite', test_data)
-
-
-
-# Load
-#module_spec = hub.load_module_spec("./model_files/")
-#height, width = hub.get_expected_image_size(module_spec)
-#images = ...  # A batch of images with shape [batch_size, height, width, 3].
-#module = hub.Module(module_spec)
-#features = module(images)   # A batch with shape [batch_size, num_features].
-# Build
-
-# Evaluate
-
-# Export model
-
-# Draw learning curves
-def draw_learning_curves(model):
-    plt.plot(model.hitory['loss'], color='b')
-    plt.plot(model.history['val_loss'], color='r')
+    '''
+    plt.plot(history['loss'], color='b')
+    plt.plot(history['accuracy'], color='r')
     plt.show()
 
-    plt.plot(model.hitory['binary_accuracy'], color='b')
-    plt.plot(model.hitory['val_binary_accuracy'], color='r')
+    plt.plot(history.history['val_loss'], color='b')
+    plt.plot(history['val_accuracy'], color='r')
     plt.show()
 
-    plt.plot(model.hitory['mean_square_error'], color='b')
-    plt.plot(model.hitory['val_mean_squared_error'], color='r')
+    plt.plot(history['mean_square_error'], color='b')
+    plt.plot(history['val_mean_squared_error'], color='r')
     plt.show()
+    '''
 
 if __name__ == '__main__':
-
+    '''
     path_ir = '../saue bilder/Combined/IR_originale/'
 
     path = '../saue bilder/Combined/Visual_originale/'
@@ -156,66 +205,71 @@ if __name__ == '__main__':
     path_4 = '../saue bilder/Combined/Visual/'
 
     path = path
+    '''
 
-    #tf.compat.v1.image.crop_and_resize
+    for i, (k, v) in enumerate(return_image_path_dict().items()):
+        
+        #tf.compat.v1.image.crop_and_resize
 
-    split_trainrest_ratio = 0.8
-    split_testval_ratio = 0.5
-    
-    model_spec = 'efficientnet_lite4' # 'efficientnet_lite0-4 'mobilenet_v2' 'resnet_50'
-    epochs = 2
-    dropout_rate = 0.5
-    learning_rate = None
-    shuffle = True
-    batch_size = None
-    use_augmentation = True
-    train_whole_model = False
 
-    # Load Data
-    train_data, test_data, validation_data = load_data_tflite_model_maker(path, split_trainrest_ratio, split_testval_ratio)
+        path = v
+        #model_title = str(path.split("/")[-2]) #path must end with /
 
-    #test = train_data._dataset.take(2)
-    #test_2 = test_data._dataset.take(2)
+        split_trainrest_ratio = 0.8
+        split_testval_ratio = 0.5
+        model_spec = 'efficientnet_lite4' # 'efficientnet_lite0-4 'mobilenet_v2' 'resnet_50'
+        epochs = 2
+        dropout_rate = 0.5
+        learning_rate = None
+        shuffle = True
+        batch_size = None
+        use_augmentation = True
+        train_whole_model = False
 
-    # Build model 
-    #modelx_ = image_classifier.create(train_data)
-    model = build_model(
-        train_d=train_data, 
-        validation_data=validation_data, 
-        model_spec=model_spec, 
-        epochs=epochs,
-        batch_size=batch_size,
-        use_augmentation=use_augmentation,
-        train_whole_model=train_whole_model,
-        )
-    #model.train( train_data, validation_data, hparams=None)
-    model.summary()
+        # Load Data
+        train_data, test_data, validation_data = load_data_tflite_model_maker(path, split_trainrest_ratio, split_testval_ratio)
 
-    # Evaluate model
-    loss, accuracy = evaluate_model(test_data=test_data, model=model)
+        # Build model 
+        model = build_model(
+            train_d=train_data, 
+            validation_data=validation_data, 
+            model_spec=model_spec, 
+            epochs=epochs,
+            batch_size=batch_size,
+            use_augmentation=use_augmentation,
+            train_whole_model=train_whole_model,
+            )
 
-    #plot classified pictures "Manual checking"
-    xx = model.train(train_data)
-    xx.history
-    print(xx.history)
-    model.predict_top_k(test_data)
-    #test_data
-    plot_classified_images(model=model, test_data=test_data)
+        # Evaluate model
+        loss, accuracy = evaluate_model(test_data=test_data, model=model)
 
-    # Export model
-    export_model(
-        model, 
-        export_dir='model_files/', 
-        label_filename='classification_labels', 
-        overwrite=True,
-        export_format=ExportFormat.SAVED_MODEL,
-        save_model_filename=str('tflite_model_saved')
+        # Plot model
+        draw_learning_curves(model.history, k)
 
-        )
-    #draw_learning_curves(model)
+        
+        #xx = model.train(train_data)
+        #xx.history
+        #print(xx.history)
+        #asd = model.predict_top_k(test_data)
+        #print(asd)
+        
+        #plot classified pictures "Manual checking"
+        #plot_classified_images(model=model, test_data=test_data)
 
-    print("done done")
-    quit()
+        # Export model
+        export_model(
+            model, 
+            export_dir='model_files/', 
+            label_filename=str(k + "_labels"), 
+            overwrite=True,
+            export_format=ExportFormat.SAVED_MODEL,
+            save_model_filename=str(k + "_model_saved")
+
+            )
+        #draw_learning_curves(model)
+
+        print("done done")
+        
 
 
 
